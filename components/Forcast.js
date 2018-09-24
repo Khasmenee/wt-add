8498967d9641be5a9b5fe844e4b20af928333a4a
Khasmenee <camera_menee@hotmail.com>
2018-09-24 11:59:47 +0700

    first commit

---
+++ components/Forcast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View , StyleSheet } from 'react-native';
+export default class Forecast extends React.Component {
+    render() {
+    return (
+    <View>
+    <Text>{this.props.main}</Text>
+    <Text>{this.props.description}</Text>
+    <Text>{this.props.temp}</Text>
+    <Text>°C</Text>
+    </View>
+    );
+    }
+   }
+   const styles = StyleSheet.create({
+    container: { paddingTop: 25 },
+    backdrop: { width: '100%', height: '100%'},
+   });
+   
